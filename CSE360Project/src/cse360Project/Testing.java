89dfcad5c6483a1d275fe53bede6ff574fb0be9b
Carlos Hernandez <carloshernandez4045@gmail.com>
2024-10-09 23:53:02 -0700

    test

--- CSE360Project/src/cse360Project/Testing.java
+++ CSE360Project/src/cse360Project/Testing.java
@@ -14,13 +14,20 @@ import cse360Project.Password;
  * 
  */
 public class Testing {
-	public static void main(String[] args) throws Exception
-	{
-		var pass = new Password("Someone1");
-		System.out.println(pass.hashedPassword);
-		System.out.println(pass.randSalt);
-		
-		System.out.println(pass.verifyPassword("Someone"));
-		System.out.println(pass.verifyPassword("Someone1"));
+	public static void test() throws Exception {
+		// Create a Password object and hash the password "Someone1"
+		Password pass = new Password("Someone1");
+
+		// Print out the hashed password and salt (these will be in byte array format)
+		System.out.println("Hashed Password (Base64 Encoded): " + java.util.Base64.getEncoder().encodeToString(pass.hashedPassword));
+		System.out.println("Random Salt (Base64 Encoded): " + java.util.Base64.getEncoder().encodeToString(pass.randSalt));
+
+		// Verify if the password "Someone" is correct (expected to fail)
+		boolean isVerifiedIncorrect = pass.verifyPassword("Someone");
+		System.out.println("Verification of 'Someone': " + (isVerifiedIncorrect ? "Success" : "Failure"));
+
+		// Verify if the password "Someone1" is correct (expected to pass)
+		boolean isVerifiedCorrect = pass.verifyPassword("Someone1");
+		System.out.println("Verification of 'Someone1': " + (isVerifiedCorrect ? "Success" : "Failure"));
 	}
 }
